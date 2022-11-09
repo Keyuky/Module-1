35b524945112fad701e2e4b2cebf603a0b55a3db
Keyuky <nguyenminhvu031202@gmail.com>
2022-11-09 11:29:21 +0700

    ver 2

---
+++ module-01-bootcamp-preparation/CTNC/CTNC.js
@@ -0,0 +1,25 @@
+function Add() {
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+    let result = Number(number1) + Number(number2);
+    document.getElementById("result").innerHTML = result;
+}
+function Sub() {
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+    let result = Number(number1) - Number(number2);
+    document.getElementById("result").innerHTML = result;
+}
+function Multi() {
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+    let result = Number(number1) * Number(number2);
+    document.getElementById("result").innerHTML = result;
+}
+function Div() {
+    let number1 = document.getElementById("num1").value;
+    let number2 = document.getElementById("num2").value;
+    let result = Number(number1) / Number(number2);
+    document.getElementById("result").innerHTML = result;
+}
+
